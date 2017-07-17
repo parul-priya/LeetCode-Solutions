27f65d897a5b5fefe217532b441c4d2bd1f61042
Parul Priya <parulpriya30@tamu.edu>
2017-07-17 14:10:22 -0500

    Add ReverseLinkedList.py

---
+++ ReverseLinkedList.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        
+        if not head or not head.next : return head
+        
+        prev = None
+        curr = head
+        nextnode = None
+        
+        while curr :
+            nextnode = curr.next
+            curr.next = prev
+            
+            prev, curr = curr, nextnode
+        return prev
