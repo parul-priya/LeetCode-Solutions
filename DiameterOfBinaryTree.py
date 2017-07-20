aa79649ebab0378ca4286c25be13ecc04f238839
Parul Priya <parulpriya30@tamu.edu>
2017-07-19 20:26:48 -0500

    Add DiameterOfBinaryTree.py

---
+++ DiameterOfBinaryTree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root : return 0
+        
+        ans = []
+        stack = []
+        while root or stack :
+            if root : 
+                stack.append(root)
+                root = root.left
+            else :
+                root = stack.pop()
+                ans.append(1 + self.depth(root.left) + self.depth(root.right))
+                root = root.right
+        return max(ans) - 1
+        
+    def depth(self, root) :
+        if not root : return 0 
+        return 1 + max(self.depth(root.left), self.depth(root.right))
